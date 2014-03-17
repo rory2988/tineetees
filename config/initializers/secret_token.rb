a26bcb281655f1403173ac4ba085c9f588e1eeef
h4w5 <philip@h4w5.com>
2014-03-17 11:52:19 -0400

    new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tineetees::Application.config.secret_key_base = '7402d25249dc9c09e2b8f890fc1921547136548d5fbf5667e9438cf387aff756b48af7ec904b63e22c794d58b669211f34f20d388572dbf6cde819301883ba81'
