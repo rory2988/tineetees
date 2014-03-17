6281f6ca7ff7b6215c3072cba96c6e4a13c8ca6e
kdoyne <kdoyne930@gmail.com>
2014-03-17 16:55:36 -0400

    bcrypt, need to fix email test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_many :products, through: :products_purchases
   validates :email, presence: :true, uniqueness: :true 
   has_secure_password
+  validates :password_digest, presence: :true
 end
