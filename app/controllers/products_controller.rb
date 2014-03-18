76b5f7227111c539753c85317168e7a29aaefc76
Jennifer <jennifergapay@gmail.com>
2014-03-18 14:04:50 -0400

    add product view page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,15 @@
 class ProductsController < ApplicationController
 
-  def show
-
-    binding.pry
-
+  def index
   end
 
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+end
+
+def product_params
+  params.permit(:name, :description, :image, :price, :sold, :on_hand, :on_order)
 end
